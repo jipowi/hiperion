26c6c79790eea1c0298d47ca28e77662eafb01f4
paulJimenez666 <paul_jimenez777@hotmail.com>
2015-01-05 22:46:04 -0500

    Modulo de seguridad

--- hiperion_web/src/main/java/ec/com/avila/emision/web/backings/AccidentesPersonalesBacking.java
+++ hiperion_web/src/main/java/ec/com/avila/emision/web/backings/AccidentesPersonalesBacking.java
@@ -4,6 +4,7 @@
 package ec.com.avila.emision.web.backings;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -11,6 +12,7 @@ import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
+import javax.faces.model.SelectItem;
 
 import org.apache.log4j.Logger;
 
@@ -20,12 +22,16 @@ import ec.com.avila.emision.web.domain.ClausulaAdicional;
 import ec.com.avila.emision.web.domain.Cobertura;
 import ec.com.avila.emision.web.domain.CondicionEspecial;
 import ec.com.avila.hiperion.comun.HiperionException;
+import ec.com.avila.hiperion.emision.entities.Catalogo;
 import ec.com.avila.hiperion.emision.entities.DetalleAnexo;
+import ec.com.avila.hiperion.emision.entities.DetalleCatalogo;
 import ec.com.avila.hiperion.emision.entities.Ramo;
 import ec.com.avila.hiperion.emision.entities.RamoAccidentesPersonale;
+import ec.com.avila.hiperion.servicio.CatalogoService;
 import ec.com.avila.hiperion.servicio.RamoAccidentesPersonalesService;
 import ec.com.avila.hiperion.servicio.RamoService;
 import ec.com.avila.hiperion.web.beans.RamoBean;
+import ec.com.avila.hiperion.web.util.HiperionMensajes;
 
 /**
  * <b>Clase Baking que permite gestionar la informaci&oacute;n que se maneje en las p&acute;ginas web que utilicen el Ramo ACCIDENTES PERSONALES. </b>
@@ -45,6 +51,9 @@ public class AccidentesPersonalesBacking implements Serializable {
 
 	@EJB
 	private RamoAccidentesPersonalesService ramoAccidentesPersonalesService;
+	
+	@EJB
+	private CatalogoService catalogoService;
 
 	@ManagedProperty(value = "#{ramoBean}")
 	private RamoBean ramoBean;
@@ -62,6 +71,8 @@ public class AccidentesPersonalesBacking implements Serializable {
 	private List<Cobertura> selectCoberturas;
 	private List<CondicionEspecial> condicionesEspeciales;
 	private List<CondicionEspecial> selectCondicionesEspeciales;
+	private List<SelectItem> sexoItems;
+	
 
 	Logger log = Logger.getLogger(AccidentesPersonalesBacking.class);
 
@@ -233,6 +244,32 @@ public class AccidentesPersonalesBacking implements Serializable {
 	 */
 	public void setRamoAccidentesPersonalesBean(RamoAccidentesPersonalesBean ramoAccidentesPersonalesBean) {
 		this.ramoAccidentesPersonalesBean = ramoAccidentesPersonalesBean;
+	}
+
+	/**
+	 * @return the sexoItems
+	 * @throws HiperionException 
+	 */
+	public List<SelectItem> getSexoItems() throws HiperionException {
+		this.sexoItems = new ArrayList<SelectItem>();
+		Catalogo catalogo = catalogoService.consultarCatalogoById(HiperionMensajes.getInstancia().getLong(
+				"ec.gob.avila.hiperion.recursos.catalogoSexo"));
+		List<DetalleCatalogo> sexos = catalogo.getDetalleCatalogos();
+
+		for (DetalleCatalogo detalle : sexos) {
+			SelectItem selectItem = new SelectItem(detalle.getCodDetalleCatalogo(), detalle.getDescDetCatalogo());
+			sexoItems.add(selectItem);
+		}
+
+		return sexoItems;
+	}
+
+	/**
+	 * @param sexoItems
+	 *            the sexoItems to set
+	 */
+	public void setSexoItems(List<SelectItem> sexoItems) {
+		this.sexoItems = sexoItems;
 	}
 
 }
