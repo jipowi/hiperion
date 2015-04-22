0144985f109babf5d584c189fd4c8d10498a1947
franklinPozo <fpdany18@hotmail.com>
2015-04-21 22:01:57 -0500

    Creacion

--- hiperion_ejb/src/main/java/ec/com/avila/hiperion/doc/servicio/GenerarDocBuenaCalidadMateriales.java
+++ hiperion_ejb/src/main/java/ec/com/avila/hiperion/doc/servicio/GenerarDocBuenaCalidadMateriales.java
@@ -8,29 +8,26 @@ import javax.ejb.Local;
 import ec.com.avila.hiperion.comun.HiperionException;
 import ec.com.avila.hiperion.emision.entities.RamoBuenaCalMat;
 
-/** 
- * <b>
- *  Interface de generación del documento PDF
- * </b>
- *  
+/**
+ * <b> Interface de generación del documento PDF </b>
+ * 
  * @author Franklin Pozo
  * @version 1.0,17/03/2015
  * @since JDK1.6
  */
 @Local
 public interface GenerarDocBuenaCalidadMateriales {
-	
-	
+
 	/**
 	 * 
-	 * <b>
-	 * Permite generar elXML del ramo agropecuario
-	 * </b>
-	 * <p>[Author: Avila Franklin Pozo, Date: 17/03/2015]</p>
-	 *
+	 * <b> Permite generar elXML del ramo agropecuario </b>
+	 * <p>
+	 * [Author: Avila Franklin Pozo, Date: 17/03/2015]
+	 * </p>
+	 * 
 	 * @param buenaCalMat
 	 * @return
 	 * @throws HiperionException
 	 */
-String generarXmlBuenaCalidadMateriales(RamoBuenaCalMat buenaCalMat) throws HiperionException;
+	public String generarXmlBuenaCalidadMateriales(RamoBuenaCalMat buenaCalMat) throws HiperionException;
 }
