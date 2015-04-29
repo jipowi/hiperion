cf912cd7a2b56fb0c0493681587b7aeb507e02c5
franklinPozo <fpdany18@hotmail.com>
2015-04-28 23:56:40 -0500

    Modificacion Backing

--- hiperion_web/src/main/java/ec/com/avila/emision/web/backings/DineroValoresBacking.java
+++ hiperion_web/src/main/java/ec/com/avila/emision/web/backings/DineroValoresBacking.java
@@ -7,7 +7,9 @@ package ec.com.avila.emision.web.backings;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
@@ -32,7 +34,10 @@ import ec.com.avila.hiperion.servicio.RamoService;
 import ec.com.avila.hiperion.web.beans.RamoBean;
 import ec.com.avila.hiperion.web.beans.UsuarioBean;
 import ec.com.avila.hiperion.web.model.AnexosDataModel;
+import ec.com.avila.hiperion.web.util.ConstantesUtil;
+import ec.com.avila.hiperion.web.util.GenerarPdfUtil;
 import ec.com.avila.hiperion.web.util.HiperionMensajes;
+import ec.com.avila.hiperion.web.util.JsfUtil;
 import ec.com.avila.hiperion.web.util.MessagesController;
 
 /**
@@ -259,5 +264,33 @@ public class DineroValoresBacking implements Serializable {
 	 */
 	public void setSelectClausulasAdicionales(DetalleAnexoBean[] selectClausulasAdicionales) {
 		this.selectClausulasAdicionales = selectClausulasAdicionales;
+	}
+	
+	/**
+	 * 
+	 * <b>
+	 * Permite generar y descargar informacion del Ramo Dinero y Valores
+	 * </b>
+	 * <p>[Author: Franklin Pozo B, Date: 27/04/2015]</p>
+	 *
+	 * @throws HiperionException
+	 */
+	public void descargarDineroValoresPDF()throws HiperionException{
+		
+		try {
+			Map<String, Object> parametrosReporte = new HashMap<String, Object>();
+
+			parametrosReporte.put(ConstantesUtil.CONTENT_TYPE_IDENTIFICADOR, ConstantesUtil.CONTENT_TYPE_PDF);
+			parametrosReporte.put(ConstantesUtil.NOMBRE_ARCHIVO_IDENTIFICADOR, usuarioBean.getSessionUser().getIdentificacionUsuario());
+
+			parametrosReporte.put(ConstantesUtil.CONTENIDO_BYTES_IDENTIFICADOR,GenerarPdfUtil.generarAchivoPDFDineroValores(ramoDineroValores));
+
+			JsfUtil.setSessionAttribute(ConstantesUtil.PARAMETROS_DESCARGADOR_IDENTIFICADOR, parametrosReporte);
+			JsfUtil.downloadFile();
+
+		} catch (Exception e) {
+			log.error("Error al momento generar el a hoja de vida en PDF", e);
+			throw new HiperionException(e);
+		}
 	}
 }
