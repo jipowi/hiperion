613bdf91ba63187b3ef7b23fa115ea5b618b1513
paulJimenez666 <paul_jimenez777@hotmail.com>
2015-06-10 22:38:37 -0400

    Guardar ramo

---
+++ hiperion_core/src/main/java/ec/com/avila/hiperion/dao/impl/ClausulaAddAgroDaoImpl.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2014 JIPOVI Solutions - ECUADOR 
+ * Todos los derechos reservados
+ */
+package ec.com.avila.hiperion.dao.impl;
+
+import javax.ejb.Stateless;
+
+import ec.com.avila.hiperion.dao.ClausulaAddAgroDao;
+import ec.com.avila.hiperion.emision.entities.ClausulasAddAgro;
+
+/**
+ * 
+ * <b> Permite implementar las operaciones de la tabla ClausulasAddAgro </b>
+ * 
+ * @author Susana Diaz
+ * @version 1.0,15/01/14
+ * @since JDK1.6
+ */
+@Stateless
+public class ClausulaAddAgroDaoImpl extends GenericDAOImpl<ClausulasAddAgro, Long> implements ClausulaAddAgroDao {
+
+}
---
+++ hiperion_core/src/main/java/ec/com/avila/hiperion/dao/impl/CoberturaAgroDaoImpl.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2014 JIPOVI Solutions - ECUADOR 
+ * Todos los derechos reservados
+ */
+package ec.com.avila.hiperion.dao.impl;
+
+import javax.ejb.Stateless;
+
+import ec.com.avila.hiperion.dao.CoberturaAgroDao;
+import ec.com.avila.hiperion.emision.entities.CobertAgro;
+
+/**
+ * 
+ * <b> Permite implementar las operaciones de la tabla CobertAgro </b>
+ * 
+ * @author Susana Diaz
+ * @version 1.0,15/01/14
+ * @since JDK1.6
+ */
+@Stateless
+public class CoberturaAgroDaoImpl extends GenericDAOImpl<CobertAgro, Long> implements CoberturaAgroDao {
+
+}
