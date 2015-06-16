00d191c9cd72e892d760af7105c40c830fb40b78
paulJimenez666 <paul_jimenez777@hotmail.com>
2015-06-16 09:58:41 -0500

    Guardar ramo

--- hiperion_web/src/main/java/ec/com/avila/emision/web/backings/AsistenciaMedicaBacking.java
+++ hiperion_web/src/main/java/ec/com/avila/emision/web/backings/AsistenciaMedicaBacking.java
@@ -13,15 +13,16 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.model.SelectItem;
 
-import ec.com.avila.emision.web.beans.DetalleAnexoBean;
 import ec.com.avila.hiperion.comun.HiperionException;
+import ec.com.avila.hiperion.dto.LimitesCostosDTO;
 import ec.com.avila.hiperion.emision.entities.Catalogo;
 import ec.com.avila.hiperion.emision.entities.DetalleAnexo;
 import ec.com.avila.hiperion.emision.entities.DetalleCatalogo;
+import ec.com.avila.hiperion.emision.entities.LimitesCostosAsm;
 import ec.com.avila.hiperion.emision.entities.Ramo;
 import ec.com.avila.hiperion.servicio.CatalogoService;
+import ec.com.avila.hiperion.servicio.RamoAstMedicaService;
 import ec.com.avila.hiperion.servicio.RamoService;
-import ec.com.avila.hiperion.web.model.AnexosDataModel;
 import ec.com.avila.hiperion.web.util.HiperionMensajes;
 
 /**
@@ -40,19 +41,22 @@ public class AsistenciaMedicaBacking implements Serializable {
 
 	@EJB
 	private RamoService ramoService;
-	
+
+	@EJB
+	private RamoAstMedicaService ramoAstMedicaService;
+
 	@EJB
 	private CatalogoService catalogoService;
 
-	private AnexosDataModel anexosDataModel;
 	private List<DetalleAnexo> anexos;
 
-	private List<DetalleAnexoBean> limitesCostosBeneficios;
-	private List<DetalleAnexoBean> limitesCostosBenAdd;
-	private List<DetalleAnexoBean> limitesCostosMaternidad;
-	private DetalleAnexoBean[] selectLimitesCostosBeneficios;
-	private DetalleAnexoBean[] selectLimitesCostosBenAdd;
-	private DetalleAnexoBean[] selectLimitesCostosMaternidad;
+	private List<LimitesCostosAsm> limitesCostosBeneficios;
+	private List<LimitesCostosAsm> limitesCostosBenAdd;
+	private List<LimitesCostosAsm> limitesCostosMaternidad;
+	private List<LimitesCostosDTO> limitesCostosBeneficiosDTO = new ArrayList<>();
+	private List<LimitesCostosDTO> limitesCostosBenAddDTO = new ArrayList<>();
+	private List<LimitesCostosDTO> limitesCostosMaternidadDTO = new ArrayList<>();
+
 	private List<SelectItem> parentescoItems;
 
 	@PostConstruct
@@ -60,6 +64,11 @@ public class AsistenciaMedicaBacking implements Serializable {
 		try {
 			Ramo ramo = ramoService.consultarRamoPorCodigo("AM");
 			anexos = ramo.getDetalleAnexos();
+
+			obtenerLimitesCostosBeneficios();
+			obtenerLimitesCostosBenAdd();
+			obtenerLimitesCostosMaternidad();
+
 		} catch (HiperionException e) {
 			e.printStackTrace();
 		}
@@ -73,139 +82,173 @@ public class AsistenciaMedicaBacking implements Serializable {
 	 * </p>
 	 * 
 	 * @return
+	 * @throws HiperionException
 	 */
-	public AnexosDataModel obtenerLimitesCostosBeneficios() {
-		limitesCostosBeneficios = new ArrayList<DetalleAnexoBean>();
+	public void obtenerLimitesCostosBeneficios() throws HiperionException {
+		limitesCostosBeneficios = new ArrayList<LimitesCostosAsm>();
 		if (anexos != null && anexos.size() > 0) {
 			for (DetalleAnexo anexo : anexos) {
-				if (anexo.getAnexo().getIdAnexo() == 4 && anexo.getTitulo().getIdTitulo() == 3)
-					limitesCostosBeneficios.add(new DetalleAnexoBean(anexo.getIdDetalleAnexo(), anexo.getNombreDetalleAnexo()));
-			}
 
-			anexosDataModel = new AnexosDataModel(limitesCostosBeneficios);
+				Long idTitulo = ramoAstMedicaService.consultarIdTitulo(anexo.getIdDetalleAnexo());
+
+				if (anexo.getAnexo().getIdAnexo() == 4 && idTitulo == 3) {
+					LimitesCostosAsm limiteCosto = new LimitesCostosAsm();
+					limiteCosto.setLimiteCosto(anexo.getNombreDetalleAnexo());
+
+					limitesCostosBeneficios.add(limiteCosto);
+				}
+			}
+			for (LimitesCostosAsm limite : limitesCostosBeneficios) {
+				LimitesCostosDTO limiteDTO = new LimitesCostosDTO();
+				limiteDTO.setLimitesCostos(limite.getLimiteCosto());
+				limiteDTO.setSeleccion(false);
+
+				limitesCostosBeneficiosDTO.add(limiteDTO);
+			}
 		}
 
-		return anexosDataModel;
 	}
 
 	/**
 	 * 
-	 * <b> Permite obtener los Limites y Costos con titulo Maternidad del Ramo Asistencia Medica. </b>
+	 * <b> Permite obtener los Limites y Costos con titulo Beneficios Add del Ramo Asistencia Medica </b>
 	 * <p>
-	 * [Author: Dario Vinueza, Date: 20/04/2014]
+	 * [Author: Paul Jimenez, Date: 16/06/2015]
 	 * </p>
 	 * 
-	 * @return
+	 * @throws HiperionException
 	 */
-	public AnexosDataModel obtenerLimitesCostosBenAdd() {
-		limitesCostosBenAdd = new ArrayList<DetalleAnexoBean>();
+	public void obtenerLimitesCostosBenAdd() throws HiperionException {
+		limitesCostosBenAdd = new ArrayList<LimitesCostosAsm>();
 		if (anexos != null && anexos.size() > 0) {
 			for (DetalleAnexo anexo : anexos) {
-				if (anexo.getAnexo().getIdAnexo() == 4 && anexo.getTitulo().getIdTitulo() == 4)
-					limitesCostosBenAdd.add(new DetalleAnexoBean(anexo.getIdDetalleAnexo(), anexo.getNombreDetalleAnexo()));
-			}
 
-			anexosDataModel = new AnexosDataModel(limitesCostosBenAdd);
+				Long idTitulo = ramoAstMedicaService.consultarIdTitulo(anexo.getIdDetalleAnexo());
+
+				if (anexo.getAnexo().getIdAnexo() == 4 && idTitulo == 4) {
+
+					LimitesCostosAsm limiteCosto = new LimitesCostosAsm();
+					limiteCosto.setLimiteCosto(anexo.getNombreDetalleAnexo());
+
+					limitesCostosBenAdd.add(limiteCosto);
+				}
+			}
+			for (LimitesCostosAsm limite : limitesCostosBenAdd) {
+				LimitesCostosDTO limiteDTO = new LimitesCostosDTO();
+				limiteDTO.setLimitesCostos(limite.getLimiteCosto());
+				limiteDTO.setSeleccion(false);
+
+				limitesCostosBenAddDTO.add(limiteDTO);
+			}
 		}
 
-		return anexosDataModel;
 	}
 
 	/**
 	 * 
-	 * <b> Permite obtener los Limites y Costos con titulo Beneficios Adicionales del Ramo Asistencia Medica. </b>
+	 * <b> Permite obtener los Limites y Costos con titulo de Materniadad del Ramo Asistencia Medica. </b>
 	 * <p>
-	 * [Author: Dario Vinueza, Date: 20/04/2014]
+	 * [Author: Paul Jimenez, Date: 16/06/2015]
 	 * </p>
 	 * 
-	 * @return
+	 * @throws HiperionException
 	 */
-	public AnexosDataModel obtenerLimitesCostosMaternidad() {
-		limitesCostosMaternidad = new ArrayList<DetalleAnexoBean>();
+	public void obtenerLimitesCostosMaternidad() throws HiperionException {
+		limitesCostosMaternidad = new ArrayList<LimitesCostosAsm>();
 		if (anexos != null && anexos.size() > 0) {
 			for (DetalleAnexo anexo : anexos) {
-				if (anexo.getAnexo().getIdAnexo() == 4 && anexo.getTitulo().getIdTitulo() == 5)
-					limitesCostosMaternidad.add(new DetalleAnexoBean(anexo.getIdDetalleAnexo(), anexo.getNombreDetalleAnexo()));
-			}
 
-			anexosDataModel = new AnexosDataModel(limitesCostosMaternidad);
+				Long idTitulo = ramoAstMedicaService.consultarIdTitulo(anexo.getIdDetalleAnexo());
+
+				if (anexo.getAnexo().getIdAnexo() == 4 && idTitulo == 5) {
+
+					LimitesCostosAsm limiteCosto = new LimitesCostosAsm();
+					limiteCosto.setLimiteCosto(anexo.getNombreDetalleAnexo());
+
+					limitesCostosMaternidad.add(limiteCosto);
+				}
+			}
+			for (LimitesCostosAsm limite : limitesCostosMaternidad) {
+				LimitesCostosDTO limiteDTO = new LimitesCostosDTO();
+				limiteDTO.setLimitesCostos(limite.getLimiteCosto());
+				limiteDTO.setSeleccion(false);
+
+				limitesCostosMaternidadDTO.add(limiteDTO);
+			}
 		}
 
-		return anexosDataModel;
 	}
 
 	public void guardar() {
 	}
 
 	/**
-	 * @return the selectLimitesCostosBeneficios
-	 */
-	public DetalleAnexoBean[] getSelectLimitesCostosBeneficios() {
-		return selectLimitesCostosBeneficios;
-	}
-
-	/**
-	 * @param selectLimitesCostosBeneficios
-	 *            the selectLimitesCostosBeneficios to set
-	 */
-	public void setSelectLimitesCostosBeneficios(DetalleAnexoBean[] selectLimitesCostosBeneficios) {
-		this.selectLimitesCostosBeneficios = selectLimitesCostosBeneficios;
-	}
-
-	/**
-	 * @return the selectLimitesCostosBenAdd
-	 */
-	public DetalleAnexoBean[] getSelectLimitesCostosBenAdd() {
-		return selectLimitesCostosBenAdd;
-	}
-
-	/**
-	 * @param selectLimitesCostosBenAdd
-	 *            the selectLimitesCostosBenAdd to set
-	 */
-	public void setSelectLimitesCostosBenAdd(DetalleAnexoBean[] selectLimitesCostosBenAdd) {
-		this.selectLimitesCostosBenAdd = selectLimitesCostosBenAdd;
-	}
-
-	/**
-	 * @return the selectLimitesCostosMaternidad
-	 */
-	public DetalleAnexoBean[] getSelectLimitesCostosMaternidad() {
-		return selectLimitesCostosMaternidad;
-	}
-
-	/**
-	 * @param selectLimitesCostosMaternidad
-	 *            the selectLimitesCostosMaternidad to set
-	 */
-	public void setSelectLimitesCostosMaternidad(DetalleAnexoBean[] selectLimitesCostosMaternidad) {
-		this.selectLimitesCostosMaternidad = selectLimitesCostosMaternidad;
-	}
-
-	/**
 	 * @return the parentescoItems
 	 */
-	public List<SelectItem> getParentescoItems() throws HiperionException{
+	public List<SelectItem> getParentescoItems() throws HiperionException {
 		this.parentescoItems = new ArrayList<SelectItem>();
-		Catalogo catalogo = catalogoService.consultarCatalogoById(HiperionMensajes.getInstancia().getLong(
-				"ec.gob.avila.hiperion.recursos.parentesco"));
+		Catalogo catalogo = catalogoService.consultarCatalogoById(HiperionMensajes.getInstancia()
+				.getLong("ec.gob.avila.hiperion.recursos.parentesco"));
 		List<DetalleCatalogo> parentesco = catalogo.getDetalleCatalogos();
-		
-		for(DetalleCatalogo detalle: parentesco){
-			SelectItem selectItem=new SelectItem(detalle.getCodDetalleCatalogo(),detalle.getDescDetCatalogo());
+
+		for (DetalleCatalogo detalle : parentesco) {
+			SelectItem selectItem = new SelectItem(detalle.getCodDetalleCatalogo(), detalle.getDescDetCatalogo());
 			parentescoItems.add(selectItem);
 		}
 		return parentescoItems;
 	}
 
 	/**
-	 * @param parentescoItems the parentescoItems to set
+	 * @param parentescoItems
+	 *            the parentescoItems to set
 	 */
 	public void setParentescoItems(List<SelectItem> parentescoItems) {
 		this.parentescoItems = parentescoItems;
 	}
 
+	/**
+	 * @return the limitesCostosBeneficiosDTO
+	 */
+	public List<LimitesCostosDTO> getLimitesCostosBeneficiosDTO() {
+		return limitesCostosBeneficiosDTO;
+	}
 
-	
-	
+	/**
+	 * @param limitesCostosBeneficiosDTO
+	 *            the limitesCostosBeneficiosDTO to set
+	 */
+	public void setLimitesCostosBeneficiosDTO(List<LimitesCostosDTO> limitesCostosBeneficiosDTO) {
+		this.limitesCostosBeneficiosDTO = limitesCostosBeneficiosDTO;
+	}
+
+	/**
+	 * @return the limitesCostosBenAddDTO
+	 */
+	public List<LimitesCostosDTO> getLimitesCostosBenAddDTO() {
+		return limitesCostosBenAddDTO;
+	}
+
+	/**
+	 * @param limitesCostosBenAddDTO
+	 *            the limitesCostosBenAddDTO to set
+	 */
+	public void setLimitesCostosBenAddDTO(List<LimitesCostosDTO> limitesCostosBenAddDTO) {
+		this.limitesCostosBenAddDTO = limitesCostosBenAddDTO;
+	}
+
+	/**
+	 * @return the limitesCostosMaternidadDTO
+	 */
+	public List<LimitesCostosDTO> getLimitesCostosMaternidadDTO() {
+		return limitesCostosMaternidadDTO;
+	}
+
+	/**
+	 * @param limitesCostosMaternidadDTO
+	 *            the limitesCostosMaternidadDTO to set
+	 */
+	public void setLimitesCostosMaternidadDTO(List<LimitesCostosDTO> limitesCostosMaternidadDTO) {
+		this.limitesCostosMaternidadDTO = limitesCostosMaternidadDTO;
+	}
+
 }
