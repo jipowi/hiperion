328982f77621b56d69a0af6974de6f9aac6ef0db
paulJimenez666 <paul_jimenez777@hotmail.com>
2015-07-27 23:13:52 -0500

    Guardar ramo

---
+++ hiperion_core/src/main/java/ec/com/avila/hiperion/dao/impl/ClausulaAddDineroDaoImpl.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2014 JIPOVI Solutions - ECUADOR 
+ * Todos los derechos reservados
+ */
+package ec.com.avila.hiperion.dao.impl;
+
+import javax.ejb.Stateless;
+
+import ec.com.avila.hiperion.dao.ClausulaAddDineroDao;
+import ec.com.avila.hiperion.emision.entities.ClausulasAddDinero;
+
+/**
+ * 
+ * <b> Permite implementar las operaciones de la tabla ClausulasAddDinero </b>
+ * 
+ * @author Susana Diaz
+ * @version 1.0,15/01/14
+ * @since JDK1.6
+ */
+@Stateless
+public class ClausulaAddDineroDaoImpl extends GenericDAOImpl<ClausulasAddDinero, Long> implements ClausulaAddDineroDao {
+
+}
---
+++ hiperion_core/src/main/java/ec/com/avila/hiperion/dao/impl/CoberturaDineroDaoImpl.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2014 JIPOVI Solutions - ECUADOR 
+ * Todos los derechos reservados
+ */
+package ec.com.avila.hiperion.dao.impl;
+
+import javax.ejb.Stateless;
+
+import ec.com.avila.hiperion.dao.CoberturaDineroDao;
+import ec.com.avila.hiperion.emision.entities.CobertDineroVal;
+
+/**
+ * 
+ * <b> Permite implementar las operaciones de la tabla CobertAccPer </b>
+ * 
+ * @author Susana Diaz
+ * @version 1.0,15/01/14
+ * @since JDK1.6
+ */
+@Stateless
+public class CoberturaDineroDaoImpl extends GenericDAOImpl<CobertDineroVal, Long> implements CoberturaDineroDao {
+
+}
