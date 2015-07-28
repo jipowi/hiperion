ec8d6d088eb97bc771dceb07fc49eba71efc2760
paulJimenez666 <paul_jimenez777@hotmail.com>
2015-07-27 22:00:06 -0500

    Guardar ramo

---
+++ hiperion_core/src/main/java/ec/com/avila/hiperion/dao/impl/CoberturaBuenaCalMatDaoImpl.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2014 JIPOVI Solutions - ECUADOR 
+ * Todos los derechos reservados
+ */
+package ec.com.avila.hiperion.dao.impl;
+
+import javax.ejb.Stateless;
+
+import ec.com.avila.hiperion.dao.CoberturaBuenaCalMatDao;
+import ec.com.avila.hiperion.emision.entities.CobertMateriale;
+
+/**
+ * 
+ * <b> Permite implementar las operaciones de la tabla CobertMateriale </b>
+ * 
+ * @author Susana Diaz
+ * @version 1.0,15/01/14
+ * @since JDK1.6
+ */
+@Stateless
+public class CoberturaBuenaCalMatDaoImpl extends GenericDAOImpl<CobertMateriale, Long> implements CoberturaBuenaCalMatDao {
+
+}
