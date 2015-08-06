a8e8369b80f75ad6481b16b8e643e04a0ade50bb
jonathantapia <jtapia1390@gmail.com>
2015-08-06 18:42:30 -0500

    Ramo Vehiculo, guardar datos en la DB

---
+++ hiperion_core/src/main/java/ec/com/avila/hiperion/dao/impl/ClausulaAddVehiculoDaoImpl.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2014 JIPOVI Solutions - ECUADOR 
+ * Todos los derechos reservados
+ */
+package ec.com.avila.hiperion.dao.impl;
+
+import javax.ejb.Stateless;
+
+import ec.com.avila.hiperion.dao.ClausulaAddAccPerDao;
+import ec.com.avila.hiperion.dao.ClausulaAddVehiculoDao;
+import ec.com.avila.hiperion.emision.entities.ClausulasAddAccPer;
+import ec.com.avila.hiperion.emision.entities.ClausulasAddVh;
+
+/**
+ * 
+ * <b> Permite implementar las operaciones de la tabla ClausulasAddVh </b>
+ * 
+ * @author Susana Diaz
+ * @version 1.0,15/01/14
+ * @since JDK1.6
+ */
+@Stateless
+public class ClausulaAddVehiculoDaoImpl extends GenericDAOImpl<ClausulasAddVh, Long> implements ClausulaAddVehiculoDao {
+
+}
---
+++ hiperion_core/src/main/java/ec/com/avila/hiperion/dao/impl/CondicionEspVehiculoDaoImpl.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2014 JIPOVI Solutions - ECUADOR 
+ * Todos los derechos reservados
+ */
+package ec.com.avila.hiperion.dao.impl;
+
+import javax.ejb.Stateless;
+
+import ec.com.avila.hiperion.dao.CondicionEspAccPerDao;
+import ec.com.avila.hiperion.dao.CondicionEspVehiculoDao;
+import ec.com.avila.hiperion.emision.entities.CondEspAccPer;
+import ec.com.avila.hiperion.emision.entities.CondEspVh;
+
+/**
+ * 
+ * <b> Permite implementar las operaciones de la tabla CondEspVh </b>
+ * 
+ * @author Susana Diaz
+ * @version 1.0,15/01/14
+ * @since JDK1.6
+ */
+@Stateless
+public class CondicionEspVehiculoDaoImpl extends GenericDAOImpl<CondEspVh, Long> implements CondicionEspVehiculoDao {
+
+}
