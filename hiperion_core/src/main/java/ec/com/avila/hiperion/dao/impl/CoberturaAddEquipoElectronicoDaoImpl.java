f28902f03bb13fa41e2525571410e7f80bed3455
paulJimenez666 <paul_jimenez777@hotmail.com>
2015-08-24 16:58:22 -0500

    Guardar ramo agosto

---
+++ hiperion_core/src/main/java/ec/com/avila/hiperion/dao/impl/CoberturaAddEquipoElectronicoDaoImpl.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2014 JIPOVI Solutions - ECUADOR 
+ * Todos los derechos reservados
+ */
+package ec.com.avila.hiperion.dao.impl;
+
+import javax.ejb.Stateless;
+
+import ec.com.avila.hiperion.dao.CoberturaAddEquipoElectronicoDao;
+import ec.com.avila.hiperion.emision.entities.CobertAddEquipo;
+
+/**
+ * 
+ * <b> Permite implementar las operaciones de la tabla CobertEquipo </b>
+ * 
+ * @author Susana Diaz
+ * @version 1.0,15/01/14
+ * @since JDK1.6
+ */
+@Stateless
+public class CoberturaAddEquipoElectronicoDaoImpl extends GenericDAOImpl<CobertAddEquipo, Long> implements CoberturaAddEquipoElectronicoDao {
+
+}
---
+++ hiperion_core/src/main/java/ec/com/avila/hiperion/dao/impl/CoberturaCumpContratoDaoImpl.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2014 JIPOVI Solutions - ECUADOR 
+ * Todos los derechos reservados
+ */
+package ec.com.avila.hiperion.dao.impl;
+
+import javax.ejb.Stateless;
+
+import ec.com.avila.hiperion.dao.CoberturaCumpContratoDao;
+import ec.com.avila.hiperion.emision.entities.CobertContrato;
+
+/**
+ * 
+ * <b> Permite implementar las operaciones de la tabla CobertContrato.
+ * 
+ * @author Paul Jimenez
+ * @version 1.0,24/08/2015
+ * @since JDK1.6
+ */
+@Stateless
+public class CoberturaCumpContratoDaoImpl extends GenericDAOImpl<CobertContrato, Long> implements CoberturaCumpContratoDao {
+
+}
