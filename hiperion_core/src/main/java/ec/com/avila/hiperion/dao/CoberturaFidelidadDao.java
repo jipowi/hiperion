d758de0fd22edc63285cdc0722d9854c92c1c0da
paulJimenez666 <paul_jimenez777@hotmail.com>
2015-08-25 10:22:44 -0500

    Guardar ramo

---
+++ hiperion_core/src/main/java/ec/com/avila/hiperion/dao/CoberturaFidelidadDao.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2014 JIPOVI Solutions - ECUADOR 
+ * Todos los derechos reservados
+ */
+package ec.com.avila.hiperion.dao;
+
+import javax.ejb.Local;
+
+import ec.com.avila.hiperion.emision.entities.CobertFidelidad;
+
+/**
+ * <b> Interface local de la tabla CobertFidelidad que permite realizar las operaciones necesarias </b>
+ * 
+ * @author Susana Diaz
+ * @version 1.0,15/01/2014
+ * @since JDK1.6
+ */
+@Local
+public interface CoberturaFidelidadDao extends GenericDAO<CobertFidelidad, Long> {
+
+}
---
+++ hiperion_core/src/main/java/ec/com/avila/hiperion/dao/impl/CoberturaAduaneraDaoImpl.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2014 JIPOVI Solutions - ECUADOR 
+ * Todos los derechos reservados
+ */
+package ec.com.avila.hiperion.dao.impl;
+
+import javax.ejb.Stateless;
+
+import ec.com.avila.hiperion.dao.CoberturaAduaneraDao;
+import ec.com.avila.hiperion.emision.entities.CobertAduanera;
+
+/**
+ * 
+ * <b> Permite implementar las operaciones de la tabla CobertAduanera </b>
+ * 
+ * @author Susana Diaz
+ * @version 1.0,15/01/14
+ * @since JDK1.6
+ */
+@Stateless
+public class CoberturaAduaneraDaoImpl extends GenericDAOImpl<CobertAduanera, Long> implements CoberturaAduaneraDao {
+
+}
