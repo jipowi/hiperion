bd062f61f2192c930227d2b2d0bca5248d571635
franklinPozo <fpdany18@hotmail.com>
2015-08-25 10:39:09 -0500

    Fpozo: Creacion daos Incendio

---
+++ hiperion_core/src/main/java/ec/com/avila/hiperion/dao/CoberturaIncendioDao.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2014 JIPOVI Solutions - ECUADOR 
+ * Todos los derechos reservados
+ */
+package ec.com.avila.hiperion.dao;
+
+import javax.ejb.Local;
+
+import ec.com.avila.hiperion.emision.entities.CobertIncendio;
+
+/**
+ * <b> Interface local de la tabla CobertIncendio que permite realizar las operaciones necesarias </b>
+ * 
+ * @author Franklin Pozo B
+ * @version 1.0,15/01/2014
+ * @since JDK1.6
+ */
+@Local
+public interface CoberturaIncendioDao extends GenericDAO<CobertIncendio, Long> {
+
+}
