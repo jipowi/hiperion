b9f803ed0cb425d472d8d892282fada142cc1475
franklinPozo <fpdany18@hotmail.com>
2015-08-25 21:25:08 -0500

    Creacion dao LCIncendio

---
+++ hiperion_core/src/main/java/ec/com/avila/hiperion/dao/impl/ClausulaAddLcIncendioDaoImpl.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2014 JIPOVI Solutions - ECUADOR 
+ * Todos los derechos reservados
+ */
+package ec.com.avila.hiperion.dao.impl;
+
+import javax.ejb.Stateless;
+
+import ec.com.avila.hiperion.dao.ClausulaAddLcIncendioDao;
+import ec.com.avila.hiperion.emision.entities.ClausulasAddLcIn;
+
+/**
+ * 
+ * <b> Permite implementar las operaciones de la tabla ClausulasAddIncendio </b>
+ * 
+ * @author Franklin Pozo B.
+ * @version 1.0,15/01/14
+ * @since JDK1.6
+ */
+@Stateless
+public class ClausulaAddLcIncendioDaoImpl extends GenericDAOImpl<ClausulasAddLcIn, Long> implements ClausulaAddLcIncendioDao {
+
+}
