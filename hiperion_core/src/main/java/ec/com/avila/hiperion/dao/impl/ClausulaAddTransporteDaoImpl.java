157d6be9838546d0cece0b70d2d81656fb6c087c
franklinPozo <fpdany18@hotmail.com>
2015-08-27 21:32:56 -0500

    Creacion Daos

---
+++ hiperion_core/src/main/java/ec/com/avila/hiperion/dao/impl/ClausulaAddTransporteDaoImpl.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2014 JIPOVI Solutions - ECUADOR 
+ * Todos los derechos reservados
+ */
+package ec.com.avila.hiperion.dao.impl;
+
+import javax.ejb.Stateless;
+
+import ec.com.avila.hiperion.dao.ClausulaAddTransporteDao;
+import ec.com.avila.hiperion.emision.entities.ClausulasAddTran;
+
+/**
+ * 
+ * <b> Permite implementar las operaciones de la tabla ClausulasAddTransporte </b>
+ * 
+ * @author Franklin Pozo B.
+ * @version 1.0,15/01/14
+ * @since JDK1.6
+ */
+@Stateless
+public class ClausulaAddTransporteDaoImpl extends GenericDAOImpl<ClausulasAddTran, Long> implements ClausulaAddTransporteDao {
+
+}
---
+++ hiperion_core/src/main/java/ec/com/avila/hiperion/dao/impl/CoberturaTransporteDaoImpl.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2014 JIPOVI Solutions - ECUADOR 
+ * Todos los derechos reservados
+ */
+package ec.com.avila.hiperion.dao.impl;
+
+import javax.ejb.Stateless;
+
+import ec.com.avila.hiperion.dao.CoberturaTransporteDao;
+import ec.com.avila.hiperion.emision.entities.CobertTran;
+
+/**
+ * 
+ * <b> Permite implementar las operaciones de la tabla CobertLcRoturaTransporte </b>
+ * 
+ * @author Franklin Pozo B.
+ * @version 1.0,15/01/14
+ * @since JDK1.6
+ */
+@Stateless
+public class CoberturaTransporteDaoImpl extends GenericDAOImpl<CobertTran, Long> implements CoberturaTransporteDao {
+
+}
