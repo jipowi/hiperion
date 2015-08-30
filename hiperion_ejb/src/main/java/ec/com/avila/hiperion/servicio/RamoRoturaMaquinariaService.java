3efb8af8f275a0f1373e2bef28e88bcbb4de7038
paulJimenez666 <paul_jimenez777@hotmail.com>
2015-08-30 11:17:48 -0500

    Modulo de seguridad

--- hiperion_ejb/src/main/java/ec/com/avila/hiperion/servicio/RamoRoturaMaquinariaService.java
+++ hiperion_ejb/src/main/java/ec/com/avila/hiperion/servicio/RamoRoturaMaquinariaService.java
@@ -9,6 +9,7 @@ import java.util.List;
 import javax.ejb.Local;
 
 import ec.com.avila.hiperion.comun.HiperionException;
+import ec.com.avila.hiperion.emision.entities.Poliza;
 import ec.com.avila.hiperion.emision.entities.RamoRoturaMaquinaria;
 
 /**
@@ -31,7 +32,7 @@ public interface RamoRoturaMaquinariaService {
 	 * @throws HiperionException
 	 */
 
-	public void guardarRamoRoturaMaquinaria(RamoRoturaMaquinaria ramoRoturaMaquinaria) throws HiperionException;
+	public void guardarRamoRoturaMaquinaria(RamoRoturaMaquinaria ramoRoturaMaquinaria, Poliza poliza) throws HiperionException;
 
 	/**
 	 * 
--- hiperion_ejb/src/main/java/ec/com/avila/hiperion/servicio/impl/RamoRoturaMaquinariaServiceImpl.java
+++ hiperion_ejb/src/main/java/ec/com/avila/hiperion/servicio/impl/RamoRoturaMaquinariaServiceImpl.java
@@ -10,9 +10,21 @@ import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
 import ec.com.avila.hiperion.comun.HiperionException;
+import ec.com.avila.hiperion.dao.ClausulaAddRoturaMaquinariaDao;
+import ec.com.avila.hiperion.dao.CoberturaAddRoturaMaquinariaDao;
+import ec.com.avila.hiperion.dao.CoberturaRoturaMaquinariaDao;
+import ec.com.avila.hiperion.dao.FinanciamientoDao;
 import ec.com.avila.hiperion.dao.ObjAsegRoturaMaquinariaDao;
+import ec.com.avila.hiperion.dao.PagoPolizaDao;
+import ec.com.avila.hiperion.dao.PolizaDao;
 import ec.com.avila.hiperion.dao.RamoRoturaMaquinariaDao;
+import ec.com.avila.hiperion.emision.entities.ClausulasAddRotura;
+import ec.com.avila.hiperion.emision.entities.CobertAddRotura;
+import ec.com.avila.hiperion.emision.entities.CobertRotura;
+import ec.com.avila.hiperion.emision.entities.Financiamiento;
 import ec.com.avila.hiperion.emision.entities.ObjAsegRotura;
+import ec.com.avila.hiperion.emision.entities.PagoPoliza;
+import ec.com.avila.hiperion.emision.entities.Poliza;
 import ec.com.avila.hiperion.emision.entities.RamoRoturaMaquinaria;
 import ec.com.avila.hiperion.servicio.RamoRoturaMaquinariaService;
 
@@ -30,9 +42,50 @@ public class RamoRoturaMaquinariaServiceImpl implements RamoRoturaMaquinariaServ
 	private RamoRoturaMaquinariaDao ramoRoturaMaquinariaDao;
 	@EJB
 	private ObjAsegRoturaMaquinariaDao objAsegRoturaMaquinariaDao;
+	@EJB
+	private PolizaDao polizaDao;
+	@EJB
+	private PagoPolizaDao pagoPolizaDao;
+	@EJB
+	private FinanciamientoDao financiamientoDao;
+	@EJB
+	private ClausulaAddRoturaMaquinariaDao clausulaAddDao;
+	@EJB
+	private CoberturaRoturaMaquinariaDao coberturaDao;
+	@EJB
+	private CoberturaAddRoturaMaquinariaDao coberturaAddDao;
 
-	public void guardarRamoRoturaMaquinaria(RamoRoturaMaquinaria ramoRoturaMaquinaria) throws HiperionException {
+	public void guardarRamoRoturaMaquinaria(RamoRoturaMaquinaria ramoRoturaMaquinaria, Poliza poliza) throws HiperionException {
+
+		PagoPoliza pagoPoliza = poliza.getPagoPoliza();
+		pagoPolizaDao.persist(pagoPoliza);
+
+		for (Financiamiento financiamiento : pagoPoliza.getFinanciamientos()) {
+			financiamiento.setPagoPoliza(pagoPoliza);
+			financiamientoDao.persist(financiamiento);
+		}
+
+		polizaDao.persist(poliza);
+
+		ramoRoturaMaquinaria.setPoliza(poliza);
+
 		ramoRoturaMaquinariaDao.persist(ramoRoturaMaquinaria);
+
+		for (ClausulasAddRotura clausula : ramoRoturaMaquinaria.getClausulasAddRoturas()) {
+			clausula.setRamoRoturaMaquinaria(ramoRoturaMaquinaria);
+			clausulaAddDao.persist(clausula);
+		}
+
+		for (CobertRotura cobertura : ramoRoturaMaquinaria.getCobertRoturas()) {
+			cobertura.setRamoRoturaMaquinaria(ramoRoturaMaquinaria);
+			coberturaDao.persist(cobertura);
+		}
+
+		for (CobertAddRotura cobertura : ramoRoturaMaquinaria.getCobertAddRoturas()) {
+			cobertura.setRamoRoturaMaquinaria(ramoRoturaMaquinaria);
+			coberturaAddDao.persist(cobertura);
+		}
+
 		for (ObjAsegRotura objeto : ramoRoturaMaquinaria.getObjAsegRoturas()) {
 			objAsegRoturaMaquinariaDao.persist(objeto);
 		}
