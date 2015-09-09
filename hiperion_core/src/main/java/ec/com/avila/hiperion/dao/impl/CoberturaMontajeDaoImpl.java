64d7be70a9a5e250517078485e468ec1c8f4439a
franklinPozo <fpdany18@hotmail.com>
2015-09-08 20:59:56 -0500

    Creacion

---
+++ hiperion_core/src/main/java/ec/com/avila/hiperion/dao/impl/CoberturaMontajeDaoImpl.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2014 JIPOVI Solutions - ECUADOR 
+ * Todos los derechos reservados
+ */
+package ec.com.avila.hiperion.dao.impl;
+
+import javax.ejb.Stateless;
+
+import ec.com.avila.hiperion.dao.CoberturaMontajeDao;
+import ec.com.avila.hiperion.dao.CoberturaTransporteDao;
+import ec.com.avila.hiperion.emision.entities.CobertMontaje;
+import ec.com.avila.hiperion.emision.entities.CobertTran;
+
+/**
+ * 
+ * <b> Permite implementar las operaciones de la tabla CobertLcRoturaTransporte </b>
+ * 
+ * @author Franklin Pozo B.
+ * @version 1.0,15/01/14
+ * @since JDK1.6
+ */
+@Stateless
+public class CoberturaMontajeDaoImpl extends GenericDAOImpl<CobertMontaje, Long> implements CoberturaMontajeDao {
+
+}
