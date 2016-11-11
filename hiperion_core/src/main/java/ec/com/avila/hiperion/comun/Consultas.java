ba361511a7b4a7aa459518c4dae9959ad50d2b6b
paulJimenez666 <paul_jimenez777@hotmail.com>
2016-11-10 22:59:27 -0500

    busqueda por RUC

--- hiperion_core/src/main/java/ec/com/avila/hiperion/comun/Consultas.java
+++ hiperion_core/src/main/java/ec/com/avila/hiperion/comun/Consultas.java
@@ -17,8 +17,8 @@ import javax.persistence.NamedQuery;
 		@NamedQuery(name = "Catalogo.findById", query = "SELECT c FROM Catalogo c WHERE c.idCatalogo =:idCatalogo"),
 		// CLIENTE
 		@NamedQuery(name = "Cliente.findByIdentificacion", query = "SELECT c FROM Cliente c WHERE c.identificacionPersona =:identificacion"),
-		@NamedQuery(name = "Cliente.findByApellido", query = "SELECT c FROM Cliente c WHERE lower(c.apellidoPaterno) like :apellido"),
-		@NamedQuery(name = "Cliente.findByNombres", query = "SELECT c FROM Cliente c WHERE lower(c.apellidoPaterno) like :apellido AND lower(c.nombrePersona) like :nombre"),
+		@NamedQuery(name = "Cliente.findByApellido", query = "SELECT c FROM Cliente c WHERE c.apellidoPaterno like :apellido"),
+		@NamedQuery(name = "Cliente.findByNombres", query = "SELECT c FROM Cliente c WHERE c.apellidoPaterno like :apellido AND lower(c.nombrePersona) like :nombre"),
 		@NamedQuery(name = "Cliente.findByRazonSocial", query = "SELECT c FROM Cliente c WHERE c.razonSocial like :razonSocial"),
 		@NamedQuery(name = "Cliente.findByAseguradora", query = "SELECT c FROM Cliente c WHERE c.aseguradora.codigoAseguradora =:aseguradora AND c.estado =:estado"),
 
